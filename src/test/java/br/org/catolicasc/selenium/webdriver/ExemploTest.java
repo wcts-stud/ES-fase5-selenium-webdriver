6a274a5737dabb073d132e5b7171a8c0b862b354
Willyam Castro <willyam.castro@catolicasc.org.br>
2019-06-10 23:40:50 -0300

    add validações

--- src/test/java/br/org/catolicasc/selenium/webdriver/ExemploTest.java
+++ src/test/java/br/org/catolicasc/selenium/webdriver/ExemploTest.java
@@ -2,6 +2,7 @@ package br.org.catolicasc.selenium.webdriver;
 
 
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 import org.junit.After;
 import org.junit.Before;
@@ -31,6 +32,8 @@ public class ExemploTest extends TestCase {
 		driver.quit();
 	}
 
+	
+	
 	@Test
 	public void testeGmail() throws Exception {
 		driver.get("http://www.gmail.com.br/");
@@ -54,15 +57,10 @@ public class ExemploTest extends TestCase {
 		 * 
 		 */
 
+			
+		// aguarda 2s para @GET pagina insercao senha
+		Thread.sleep(2000);
 		
-		
-		
-		// aguarda 1s para @GET pagina insercao senha
-		Thread.sleep(1000);		
-		
-		// wait field password
-		//wait.until(ExpectedConditions.invisibilityOfElementLocated(By.xpath("//input")));
-
 		
 		// insere a senha e "next"
 		WebElement campoDeSenha = driver.findElement(By.xpath("//input[@class='whsOnd zHQkBf']"));
@@ -70,22 +68,25 @@ public class ExemploTest extends TestCase {
 		driver.findElement(By.id("passwordNext")).click();
 		
 		System.out.println("1. Login realizado com sucesso");
-
 		
 		
 		Thread.sleep(4000);
 
 		
+		
 		/*
 		 * verificar se esta na caixa de entrada
 		 */
-		//boolean emInbox = driver.getCurrentUrl().contentEquals("https://mail.google.com/mail/#inbox");
-		// verifica se a pasta Inbox é mostrado;
-		boolean emInbox = driver.findElement(By.xpath("//a[@title='Inbox']")).isDisplayed();
-		assertTrue(emInbox);
 		
-		System.out.println("2. URL atual: " +driver.getCurrentUrl() +" = " + emInbox);
+		if ( driver.getCurrentUrl().equals("https://mail.google.com/mail/#inbox") ) {
+			System.out.println("2. Estamos na caixa de entrada");
+			assertTrue(true);
+		} else {
+			assertTrue(false);
+		}
+		
 
+		
 		
 		
 		
@@ -123,177 +124,206 @@ public class ExemploTest extends TestCase {
 		wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//*[contains(text(),'Message sent')]")));
 
 		System.out.println("3. Novo e-mail enviado");
-		
-		
-		
-		
-		
-		
-		/*
-		//Verifica se está na caixa de entrada 
-		driver.findElement(By.xpath("//a[@title='Inbox']")).isDisplayed();
-		assertTrue(emInbox);
-		*/
-		
-		
-		WebElement unreadmailvalid = driver.findElement(By.xpath("//*[@class='zA zE']"));		
-		
-		
-		//Se está na caixa de entrada então verifica se tem email 			
-		    if( unreadmailvalid.isDisplayed() == true ){		    	
-		    	//Verifica se o email contem o titulo Teste de Sistema
-		        if( unreadmailvalid.getText().contains("Teste de Sistema")){
-		        	//Verifica se o email contem o conteudo Selenium webdrive é legal!
-		        	if( unreadmailvalid.getText().contains("Selenium webdrive é legal!")){
-		        		assertTrue(true); 
-		        		System.out.println("\t E-mail está na caixa de entrada");
-		        	} else {
-		        		System.out.println("\t E-mail não está na caixa de entrada");
-		        	}
-		        }
-		  }
-		    
 
-		
-		
-		
-		
-		
-		
+
+
+		/*
+		 * Valida e-mail da caixa de entrada;
+		 */		
+
+		WebElement unreadmailvalid = driver.findElement(By.xpath("//*[@class='zA zE']"));
+
+		boolean validEmail = false;
+
+		if ( driver.getCurrentUrl().equals("https://mail.google.com/mail/#inbox")) {
+			System.out.print("\t Estamos na caixa de entrada e, ");
+			//Se está na caixa de entrada então verifica se tem email 			
+			if( unreadmailvalid.isDisplayed() == true ){		    	
+				//Verifica se o email contem o titulo Teste de Sistema
+				if( unreadmailvalid.getText().contains("me") & unreadmailvalid.getText().contains("Teste de Sistema")){
+					//Verifica se o email contem o conteudo Selenium webdrive é legal!
+					if( unreadmailvalid.getText().contains("Selenium webdrive é legal!")){
+						System.out.println("o novo e-mail está na caixa de entrada");
+						validEmail = true;
+					} else {
+						System.out.println("o novo e-mail não está na caixa de entrada");
+						validEmail = false;
+					}
+				}
+			}
+			assertTrue(validEmail); 
+		}else {
+			assertTrue(false); 
+		}		    
+
+
+
+		/*
+		 * Valida e-mail da caixa de saída;
+		 */		
+
 		// access sent messages;
 		driver.findElement(By.xpath("//a[@title='Sent']")).click();
 
+		Thread.sleep(5000);
+		
+		System.out.println(driver.getCurrentUrl());
 
-		WebElement sentmailvalid = driver.findElement(By.xpath("//*[@class='zA zE']"));		
-		
-		
-		//Se está na caixa de entrada então verifica se tem email 			
-		    if( sentmailvalid.isDisplayed() == true ){		    	
-		    	//Verifica se o email contem o titulo Teste de Sistema
-		        if( sentmailvalid.getText().contains("Teste de Sistema")){
-		        	//Verifica se o email contem o conteudo Selenium webdrive é legal!
-		        	if( sentmailvalid.getText().contains("Selenium webdrive é legal!")){
-		        		assertTrue(true); 
-		        		System.out.println("\t E-mail está na caixa de enviados");
-		        	} else {
-		        		System.out.println("\t E-mail não está na caixa de enviados");
-		        	}
-		        }
-		  }
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
+		WebElement validSentMail = driver.findElement(By.xpath("//*[@class='zA zE']"));
+
+		if ( driver.getCurrentUrl().equals("https://mail.google.com/mail/#sent")) {
+			// TODO: Entrando nessa condição e dando false
+			System.out.println("RESULT: " +validSentMail.isDisplayed());
+			//Se está na caixa de entrada então verifica se tem email 			
+			if( validSentMail.isDisplayed() == true ){		    	
+				//Verifica se o email contem o titulo Teste de Sistema
+				if( validSentMail.getText().contains("Teste de Sistema")){
+					//Verifica se o email contem o conteudo Selenium webdrive é legal!
+					if( validSentMail.getText().contains("Selenium webdrive é legal!")){
+						assertTrue(true); 
+						System.out.println("\t E-mail está na caixa de enviados");
+					} else {
+						System.out.println("\t E-mail não está na caixa de enviados");
+						assertTrue(false); 
+					}
+				}
+			}
+		} else {
+			System.out.println("Não estamos na caixa de enviados...");
+			assertTrue(false);
+		}
+
+
+
+
+
+		/*
+		 * Acessa caixa de entrada e abre o 1º e-mail não lido
+		 */
 
 		// access sent messages;
 		driver.findElement(By.xpath("//a[@title='Inbox']")).click();
-		
-				
+
+
 		// @GET all emails
-		List<WebElement> unreadmail = driver.findElements(By.xpath("//*[@class='zF']"));
-		
-		for(WebElement mail : unreadmail) {
+		List<WebElement> unreadEmails = driver.findElements(By.xpath("//*[@class='zF']"));
+
+		for(WebElement mail : unreadEmails) {
 			if( mail.isDisplayed() == true ){
 				mail.click();
 			}
 		}
 
 		System.out.println("5. E-mail recebido foi aberto");
-		
-		
-		
-		
-		// responde e-mail enviado;
+
+
+
+		/*
+		 * Responde o e-mail
+		 */		
+
+		// click button reply
 		driver.findElement(By.xpath("//div[@data-tooltip='Reply']")).click();
 
 		// preenche o corpo da mensagem;
 		driver.findElement(By.xpath("//div[@aria-label=\"Message Body\"]")).sendKeys("Ok, email lido. Obrigado.");		
-		
-		// clica para enviar o e-mail
+
+		// click send e-mail
 		driver.findElement(By.xpath("//div[text()='Send']")).click();
-		
+
 		// wait notification "message sent." 
 		wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//*[contains(text(),'Message sent.')]")));
 		//Thread.sleep(2000);
-		
+
 		System.out.println("6. E-mail foi respondido");
 
 
 
-				
-		
+		/*
+		 * Clica no ícone da lixeira p/ enviar para lixeira
+		 */
 
-		// clica no ícone da lixeira
+		// click the trash icon
 		driver.findElement(By.cssSelector(".iH > div:nth-child(1) > div:nth-child(2) > div:nth-child(3)")).click();		
-		
+
 		// wait message "Conversation moved to Trash."
 		wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//*[contains(text(),'Conversation moved to Trash.')]")));
-		
+
 		System.out.println("7. E-mail enviado para lixeira");
+
+
 		
+		/*
+		 * Acessa lixeira para verificar se o e-mail está na lixeira
+		 */		
 		
-		
-		
-		
-		//TODO: Acessar via elementos visiveis, não por URL;
-		// acessa lixeira
+		// access trash using URL
 		driver.get("https://mail.google.com/mail/#trash/");
-		
-		// wait 
+
+		// wait list e-mails
 		wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//div[@class='aeF']")));
-		
-		
-		
+
 		Thread.sleep(4000);
 		
 		
+		// TODO: Mesmo problema da linha 175
+/*
+		WebElement mailtrashvalid = driver.findElement(By.xpath("//*[@class='zA zE']"));
+		
+		System.out.println(mailtrashvalid.getText());
+
+		if ( driver.getCurrentUrl().equals("https://mail.google.com/mail/#trash")) {
+			//Se está na caixa de entrada então verifica se tem email 			
+			if( mailtrashvalid.isDisplayed() == true ){
+
+				if( mailtrashvalid.getText().contains("Teste de Sistema")){
+					
+					//Verifica se o email contem o conteudo Selenium webdrive é legal!					
+					if( mailtrashvalid.getText().contains("Ok, email lido. Obrigado.")){
+						assertTrue(true); 
+						System.out.println("\t E-mail na lixeira");
+					} else {
+						System.out.println("\t E-mail não está na lixeira");
+					}
+				}
+			}
+		}
+
+
+	*/	
+		
 		
 
-		WebElement mailtrashvalid = driver.findElement(By.xpath("//*[@class='zA zE']"));		
-		
-		
-		//Se está na caixa de entrada então verifica se tem email 			
-		    if( mailtrashvalid.isDisplayed() == true ){		    	
-		    	//Verifica se o email contem o titulo Teste de Sistema
-		        if( mailtrashvalid.getText().contains("Teste de Sistema")){
-		        	//Verifica se o email contem o conteudo Selenium webdrive é legal!
-		        	if( mailtrashvalid.getText().contains("Ok, email lido. Obrigado.")){
-		        		assertTrue(true); 
-		        		System.out.println("\t E-mail na lixeira");
-		        	} else {
-		        		System.out.println("\t E-mail não está na lixeira");
-		        	}
-		        }
-		  }
-		    
-		    
-		/*
-		
-		
 		// @GET in list mails 
 		List<WebElement> deleted = driver.findElements(By.xpath("//*[@class='zA zE']"));
+
+		for(WebElement mail : deleted) {
+			if(mail.isDisplayed() && 
+					mail.getText().contains("Teste de Sistema") 
+						&& mail.getText().contains("Ok, email lido") ) {
+				System.out.println("\t E-mail na lixeira");
+				break;
+			} else {
+				System.out.println("\t E-mail não está na lixeira");
+			}
+		}
+				
 		
+		
+		
+		
+		/*
+
+		// @GET in list mails 
+		List<WebElement> deleted = driver.findElements(By.xpath("//*[@class='zA zE']"));
+
 		for(WebElement mail : deleted) {
 			if(mail.isDisplayed()) {
 				mail.click();
 			}
 		}
-		
-		
-		
-		
-		
-		
+
+
 		
 		// wait body full mail 
 		wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//div[@class='nH if']")));
@@ -304,33 +334,27 @@ public class ExemploTest extends TestCase {
 		// TODO: Nao esta aguardando ser deletado e ja passa pra etapa de fazer logoff e quebra o teste
 		wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//span[contains(text(),'Conversation deleted forever')]")));		
 		
-		System.out.println("\t7.1- E-mail deletado da lixeira");
-		
+		System.out.println("\t7.1- E-mail deletado da lixeira");		
 
-		Thread.sleep(10000);	
-
-		
-		
-		
-		
-		
-		
+		Thread.sleep(10000);		
 		
 		
 
-		//wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//div[@class='nH if']")));
-		
+		//wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//div[@class='nH if']")));		
 
-		*/
-		
+		*/		
 		
 		
 		
 		/*
-		 * saindo da conta do Google - "signout"
+		 * Saindo da conta do Google - "signout"
 		*/
+		
 		driver.findElement(By.xpath("//a[@class='gb_x gb_Ea gb_f']")).click();
-		driver.findElement(By.xpath("//div[@class='gb_Ef gb_jb']//a[@class='gb_0 gb_Jf gb_Qf gb_qe gb_kb']")).click();
+		
+		Thread.sleep(3000);
+		
+		driver.findElement(By.xpath("//a[text()='Sign out']")).click();
 
 		System.out.println("8. Logout da conta Google concluído");
 		
@@ -338,9 +362,11 @@ public class ExemploTest extends TestCase {
 		Thread.sleep(3000);
 		
 		
+		
 		/*
-		 * valida se está na tela login
+		 * Valida se está na tela login
 		 */
+		
 		boolean telaDeLogin = driver.findElement(By.name("password")).isDisplayed();
 		assertTrue(telaDeLogin);
 		
